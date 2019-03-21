b5f86c018e2e34104658575450576c7522841360
Outvi V <git@outv.im>
2019-03-21 10:18:11 +0800

    Remake code, add QQ/TIM

--- whynotoib.js
+++ whynotoib.js
@@ -1,7 +1,9 @@
 "use strict";
 
-(function(){
-var page = `
+(function () {
+
+    // Constants
+    let page = `
 <style>
 #whynotOIB {
     transition: opacity .3s ease-in-out;
@@ -27,40 +29,71 @@ var page = `
 #whynotOIB .grey {
     color: grey
 }
-
 </style>
+
 <div id="whynotOIB">
   <h2>你好。</h2>
   <p>为什么不试试用你的浏览器（而不是微信）访问这里呢？</p>
   <p>点击右上角的三个点，然后选择“在浏览器中打开”。</p>
   <p>这能更好地保护你的隐私，也许还能更好地保护互联网。</p>
-  <p>[<a href="//librehs.github.io/whynotoib/">为什么?</a>] [<a class="grey" onclick="hideOIB()">立即隐藏</a>] <br />
+  <p>[<a href="//librehs.github.io/whynotoib/">为什么?</a>] [<a id="hideOIBRegBox" class="grey">立即隐藏</a>] <br />
   <span class="grey">（我们会在显示7秒后自动隐藏。）</span></p>
 </div>
 `
 
-let haveDone = false;
+    let haveDone = false;
+    let ua = navigator.userAgent;
+    let uaList = [
+        /(M|m)icro(M|m)essenger/,
+        /TIM\//,
+        /QQ\//
+    ];
+    let objectList = [
+        "WeixinJSBridge"
+    ]
+    let eventList = [
+        "WeixinJSBridgeReady"
+    ]
 
-if ( window.WeixinJSBridge || navigator.userAgent.search('(M|m)icro(M|m)essenger') !== -1) {
-    showOIB();
-}
+    // Execution
 
-document.addEventListener('WeixinJSBridgeReady', () => { showOIB(); })
-
-function showOIB() {
-    if (haveDone) return;
-    let elem = document.createElement("div");
-    elem.innerHTML = page;
-    document.body.appendChild(elem);
-    setTimeout( () => {document.getElementById('whynotOIB').className = "oibDisplay";}, 1000 );
-    setTimeout( () => {hideOIB(false);}, 8000 );
-    haveDone = true;
-}
-
-function hideOIB(sudden=true) {
-    if (sudden) {
-        document.getElementById('whynotOIB').className = "noTrans";
+    if (1 || searchObject() || searchUa()) {
+        showOIB();
+    } else {
+        for (i of eventList) document.addEventListener(i, () => { showOIB(); })
     }
-    document.getElementById('whynotOIB').className = "";
-}
+
+    // Functions
+
+    function searchObject() {
+        for (i of objectList) {
+            if (window[i]) return true;
+        }
+        return false;
+    }
+
+    function searchUa() {
+        for (i of uaList) {
+            if (ua.search(i) !== -1) return true;
+        }
+        return false;
+    }
+
+    function showOIB() {
+        if (haveDone) return;
+        let elem = document.createElement("div");
+        elem.innerHTML = page;
+        document.body.appendChild(elem);
+        setTimeout(() => { document.getElementById('whynotOIB').className = "oibDisplay"; }, 1000);
+        setTimeout(() => { hideOIB(false); }, 8000);
+        document.getElementById('hideOIBRegBox').addEventListener('click', () => { hideOIB(true) });
+        haveDone = true;
+    }
+
+    function hideOIB(sudden = true) {
+        if (sudden) {
+            document.getElementById('whynotOIB').className = "noTrans";
+        }
+        document.getElementById('whynotOIB').className = "";
+    }
 })()
